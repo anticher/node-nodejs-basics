d01007da02f6765f94500d05ea8d824653e1324c
anticher <anticherq@gmail.com>
2022-05-31 14:32:01 +0300

    feat: implement delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,19 @@
+import * as fs from 'fs/promises'
+import * as path from 'path'
+import { fileURLToPath } from 'url'
+import { exists } from './exists.js'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 export const remove = async () => {
-    // Write your code here 
-};
+    const filePath = path.join(__dirname, 'files', 'fileToRemove.txt')
+    const fileExists = await exists(filePath)
+    if (fileExists) {
+        await fs.unlink(filePath)
+    } else {
+        throw new Error('FS operation failed')
+    }
+}
+
+remove()
