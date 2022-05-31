95fe57498525b7820bbda9a28bf89217c8ff07da
anticher <anticherq@gmail.com>
2022-05-31 13:00:32 +0300

    feat: implement create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,19 @@
+import * as fs from 'fs/promises'
+import * as path from 'path'
+import { fileURLToPath } from 'url'
+import { exists } from './exists.js'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 export const create = async () => {
-    // Write your code here 
-};
+    const filePath = path.join(__dirname, 'files', 'fresh.txt')
+    const fileExists = await exists(filePath)
+    if (fileExists) {
+        throw new Error('FS operation failed')
+    } else {
+        await fs.writeFile(filePath, 'I am fresh and young')
+    }
+}
+
+create()
