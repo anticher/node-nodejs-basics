ceb23ac170df46f1f6b813ff94f9f6a9bc909aa9
anticher <anticherq@gmail.com>
2022-05-31 15:01:52 +0300

    feat: implement args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,12 @@
 export const parseArgs = () => {
-    // Write your code here 
-};
+    const args = process.argv.slice(2)
+    const result = []
+    args.forEach((item, index) => {
+        if (index % 2 === 0 && item.startsWith('--')) {
+            result.push(item.substring(2) + ' is ' + args[index + 1])
+        }
+    })
+    console.log(result.join(', '))
+}
+
+parseArgs()
