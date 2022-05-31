fe4e6b615cc5ff48f6ee155405449b2fcbfd6c24
anticher <anticherq@gmail.com>
2022-06-01 00:37:41 +0300

    feat: implement calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,18 @@
+import * as fs from 'fs/promises'
+import * as path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const { createHmac } = await import('crypto')
+
 export const calculateHash = async () => {
-    // Write your code here 
-};
+    const secret = 'abc'
+    const text = await fs.readFile(path.join(__dirname, 'files', 'fileToCalculateHashFor.txt'), 'utf8')
+    const hash = createHmac('sha256', secret)
+        .update(text)
+        .digest('hex')
+    console.log(hash)
+}
+
+calculateHash()
