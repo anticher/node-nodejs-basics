3bbe5661da5a9102662cb73ccabb303e243eb516
anticher <anticherq@gmail.com>
2022-06-01 13:53:18 +0300

    fix: parseEnv.js output

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,6 +1,8 @@
 export const parseEnv = () => {
     const keys = Object.keys(process.env)
-    const result = keys.filter((key) => key.startsWith('RSS_'))
+    const result = keys
+        .filter((key) => key.startsWith('RSS_'))
+        .map((item) => item + '=' + process.env[item])
     console.log(result.join('; '))
 }
 
