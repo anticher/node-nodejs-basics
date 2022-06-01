59611c68d237da222d19ef2940e8198e6cb263c7
anticher <anticherq@gmail.com>
2022-06-01 22:22:25 +0300

    feat: implement streams/write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,13 @@
+import { createWriteStream } from 'fs'
+import * as path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 export const write = async () => {
-    // Write your code here 
-};
+    const writeableStream = createWriteStream(path.join(__dirname, 'files', 'fileToWrite.txt'), 'utf8')
+    process.stdin.pipe(writeableStream)
+}
+
+write()
