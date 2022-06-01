9a2da78546fde95972b8f739cf516ea9e6e6cee9
anticher <anticherq@gmail.com>
2022-06-01 22:43:09 +0300

    feat: implement streams/transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,22 @@
+import { Transform } from 'stream'
+
+const customTransform = new Transform({
+    transform(data, encoding, callback) {
+        if (encoding !== 'buffer') {
+            return
+        }
+        const resultString = data
+            .toString('utf8')
+            .split('')
+            .reverse()
+            .join('')
+            .trim()
+        callback(null, resultString)
+    }
+})
+
 export const transform = async () => {
-    // Write your code here 
-};
+    process.stdin.pipe(customTransform).pipe(process.stdout);
+}
+
+transform()
