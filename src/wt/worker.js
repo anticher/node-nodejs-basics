2ac9de5e33a605fe69915722662e72a4d73db39b
anticher <anticherq@gmail.com>
2022-06-03 15:49:16 +0300

    fix: remove useless comment

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -3,9 +3,6 @@ export const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacc
 
 export const sendResult = () => {
     console.log('start worker with input', workerData)
-    // if (workerData === 15) {
-    //     throw new Error('error15')
-    // }
     parentPort.postMessage(nthFibonacci(workerData))
 }
 
