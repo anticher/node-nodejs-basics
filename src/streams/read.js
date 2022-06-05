ebb21b194ae69045f5a4816a4d910c45242419f6
anticher <anticherq@gmail.com>
2022-06-05 21:27:52 +0300

    style: code formatting

--- src/streams/read.js
+++ src/streams/read.js
@@ -8,7 +8,7 @@ const __dirname = path.dirname(__filename)
 export const read = async () => {
     try {
         const readableStream = createReadStream(path.join(__dirname, 'files', 'fileToRead.txt'), 'utf8')
-    readableStream.pipe(process.stdout)
+        readableStream.pipe(process.stdout)
     }
     catch (e) {
         console.log("\x1b[31m", e)
